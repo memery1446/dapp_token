edeb1789b699e9cfd5d6eba10a49eae134e1aebc
Mark Emery <markemery@Marks-MacBook-Pro.local>
2024-08-14 15:57:58 -0400

    08_14_1600

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -5,6 +5,8 @@ pragma solidity ^0.8.0;
 import "hardhat/console.sol";
 
 contract Token {
-	string public name = "My Token";
+	string public name = 'Dapp University';
+	//after first test//
+	string public symbol = 'DAPP';
 	
 }
--- test/Token.js
+++ test/Token.js
@@ -2,13 +2,26 @@ const { expect } = require('chai');
 const { ethers } = require('hardhat');
 
 describe('Token', () => {
+	
 
-	it('has a name', async () => {
+	it('has correct name', async () => {
 		//check name is correct//
 		// fetch, read, verify///
-		const Token = ethers.getContractFactory('Token') //fetch//
+		const Token = await ethers.getContractFactory('Token') //fetch//
 		let token = await Token.deploy() //deploy//
 		const name = await token.name()  //read//
-		expect(name).to.equal('My Token') //verify//
-	})
+		expect(name).to.equal('Dapp University') //verify//
+///this plus two })'s ends the first test/////
 })
+	it('has correct symbol', async () => {
+		//fetch//
+		const Token = await ethers.getContractFactory('Token')
+		let token = await Token.deploy()
+		// read //
+		const symbol = await token.symbol
+		// verify //
+		expect(symbol).to.equal('DAPP')
+	})
+		
+
+	})
