6c8d6787317bb9e266c6126876de116bb5e8db76
Mark Emery <markemery@Marks-MacBook-Pro.local>
2024-08-14 16:48:22 -0400

    08_14_16:48

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -8,5 +8,9 @@ contract Token {
 	string public name = 'Dapp University';
 	//after first test//
 	string public symbol = 'DAPP';
+	uint256 public decimals = 18;
+	uint256 public totalSupply = 1000000 * (10**decimals);  
+
+	///no ''s with int's in contract///yes '1000' in .js///
 	
 }
--- test/Token.js
+++ test/Token.js
@@ -2,26 +2,42 @@ const { expect } = require('chai');
 const { ethers } = require('hardhat');
 
 describe('Token', () => {
-	
+	let token
+
+	beforeEach(async () => {
+		const Token = await ethers.getContractFactory('Token')
+		token = await Token.deploy()
+	})
 
 	it('has correct name', async () => {
 		//check name is correct//
-		// fetch, read, verify///
-		const Token = await ethers.getContractFactory('Token') //fetch//
-		let token = await Token.deploy() //deploy//
-		const name = await token.name()  //read//
-		expect(name).to.equal('Dapp University') //verify//
-///this plus two })'s ends the first test/////
+		// fetch, read, verify///now in beforeEach///
+		////FIRST///const name = await token.name()  //read//
+		/////expect(name).to.equal('Dapp University') //verify//
+///this plus two })'s ends the first test, first time, then below/////
+		//adj after second test///longer///
+
+		expect(await token.name()).to.equal('Dapp University') //lng vsn//
 })
 	it('has correct symbol', async () => {
 		//fetch//
-		const Token = await ethers.getContractFactory('Token')
-		let token = await Token.deploy()
-		// read //
-		const symbol = await token.symbol()
-		// verify //
-		expect(symbol).to.equal('DAPP')
-	})
 		
+		// read //
+		////FIRST////const symbol = await token.symbol()
+		// verify //
+		//////expect(symbol).to.equal('DAPP')
+
+		////end of second text first time, then below adj..////
+
+		expect(await token.symbol()).to.equal('DAPP') //longer vers.//
 
 	})
+	it('has correct decimals', async () => {
+		expect(await token.decimals()).to.equal('18')
+	})
+	it('has a total supply', async () => {
+		const value = ethers.utils.parseUnits('1000000', 'ether')
+		expect(await token.totalSupply()).to.equal(value)
+	})
+})
+
