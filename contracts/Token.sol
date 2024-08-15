a861d044b1a8f7c7b7002bbc801f469b539944f1
Mark Emery <markemery@Marks-MacBook-Pro.local>
2024-08-14 20:02:15 -0400

    08_14_2002

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -5,16 +5,31 @@ pragma solidity ^0.8.0;
 import "hardhat/console.sol";
 
 contract Token {
-	string public name = 'Dapp University';
+	string public name;
 	//after first test//
-	string public symbol = 'DAPP';
-	uint256 public decimals = 18;
-	uint256 public totalSupply = 1000000 * (10**decimals);  
+	string public symbol;
+	uint256 public decimals = 18; ///this number stays///
+	uint256 public totalSupply;  
+
+
+	// Track balances
+	mapping(address => uint256) public balanceOf;
+
+
+	// Send tokens
+
+	
+	////along with above line, added msg.sender to state var's////////
 
 	constructor(string memory _name, string memory _symbol, uint256 _totalSupply) {
 		name = _name;
 		symbol = _symbol;
 		totalSupply = _totalSupply * (10**decimals);
+		/////below line comes after late test 4 stuff///
+		balanceOf[msg.sender] = totalSupply;
+		
+         ////when writing to the b.c., use [] with [msg.sender]////
+
 	}
 	
 }
--- test/Token.js
+++ test/Token.js
@@ -1,52 +1,54 @@
-///late in test 4, build constructor///
-///then describe deployment after test 4 at4/////
-
-
 const { expect } = require('chai');
 const { ethers } = require('hardhat');
 
 const tokens = (n) => {                                   
 	return ethers.utils.parseUnits(n.toString(), 'ether')
-
 }
 
 describe('Token', () => {
-	let token
+	let token, accounts, deployer
 
 	beforeEach(async () => {
 		const Token = await ethers.getContractFactory('Token')
 		token = await Token.deploy('Dapp University', 'DAPP', '1000000')
-		          ///the values above come from building constructor//
-		/////up thru test 4, skip down to it has correct name////
-	})
-////////////////////
+	
+	////lines below comes with test 5 deployer totalbal/////
+		accounts = await ethers.getSigners()//add///////////
+		deployer = accounts[0]///these to 'let' under descr//
+	})//////////////////////////////////////////////////////
+
 describe('Deployment', () => {
-/////the this container added after test 4 and constructor//
 	const name = 'Dapp University'
 	const symbol = 'DAPP'
 	const decimals = '18'
 	const totalSupply = tokens('1000000')
 
 	it('has correct name', async () => {
-		//expect(await token.name()).to.equal('Dapp University')// 
-		///above until const under desc.'depl'
 		expect(await token.name()).to.equal(name)
 	})
-	it('has correct symbol', async () => {
-		//expect(await token.symbol()).to.equal('DAPP')//
-		////read note above////
-		expect(await token.symbol()).to.equal(symbol) 
 
+	it('has correct symbol', async () => {
+		expect(await token.symbol()).to.equal(symbol) 
 	})
+
 	it('has correct decimals', async () => {
-		expect(await token.decimals()).to.equal(decimals) //note above//
+		expect(await token.decimals()).to.equal(decimals) 
 	})
 
 	it('has correct total supply', async () => {
-		expect(await token.totalSupply()).to.equal(totalSupply)//note above//
+		expect(await token.totalSupply()).to.equal(totalSupply)
+	})
+
+	it('assigns total supply to deployer', async () => {
+		
+		///cconsole.log(deployer.address)////doing this to see who dep is///
+		expect(await token.balanceOf(deployer.address)).to.equal(totalSupply)
+		///here, node//..ethers.getSigners() in beforeEach///
+
 	})
 })
 
 
-})
 
+
+})
